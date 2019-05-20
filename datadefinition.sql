d5fd2f1854f610c6b94c8097f9cc1f3c70fa9817
egozverev <zzwerruga@mail.ru>
2019-05-20 23:58:17 +0300

    Some changes

---
+++ datadefinition.sql
@@ -0,0 +1,71 @@
+DROP SCHEMA IF EXISTS coursework CASCADE;
+CREATE SCHEMA coursework;
+set search_path TO coursework, public;
+
+drop table if exists article_x_author;
+drop table if exists article_x_magazine;
+drop table if exists reviewer_x_magazine;
+drop table if exists article_x_reviewer;
+drop table if exists author_person;
+drop table if exists person;
+drop table if exists article;
+drop table if exists institute;
+drop table if exists magazine;
+
+create table person(
+  person_id INT PRIMARY KEY,
+  person_nm VARCHAR(50) NOT NULL,
+  person_age INT CHECK ( person_age >=0 ),
+  person_phone_no VARCHAR(20) CHECK(person_phone_no ~ '([-():=+]?\d[-():=+]?){10}'),
+  person_email_txt VARCHAR(50) UNIQUE ,
+  person_reviewer_flg boolean,
+  person_author_flg boolean
+);
+/* regex отбирает номера по типу '+7-914-733-5523'*/
+
+create table article(
+  article_id INT PRIMARY KEY ,
+  article_nm VARCHAR(50) NOT NULL,
+  article_subject_txt VARCHAR(50) NOT NULL ,
+  article_number_of_pages_num INT CHECK ( article_number_of_pages_num >= 0 ),
+  article_date_of_publication_dt date NOT NULL
+);
+
+create table magazine(
+  magazine_id INT PRIMARY KEY ,
+  article_nm VARCHAR(50) NOT NULL,
+  magazine_address_txt VARCHAR(100)  ,
+  magazine_site_url VARCHAR(50) UNIQUE,
+  magazine_email_txt VARCHAR(50),
+  magazine_phone_no VARCHAR(20) CHECK ( magazine_phone_no ~ '([-():=+]?\d[-():=+]?){10}'),
+  magazine_owner_nm VARCHAR(50)
+);
+
+create table institute(
+  institute_id INT PRIMARY KEY ,
+  institute_address_txt VARCHAR(100)  ,
+  institute_site_url VARCHAR(50) UNIQUE,
+  institute_email_txt VARCHAR(50),
+  institute_phone_no VARCHAR(20) CHECK ( institute_phone_no ~ '([-():=+]?\d[-():=+]?){10}')
+);
+
+create table author_person(
+  person_id INT REFERENCES person(person_id),
+  institute_id INT  REFERENCES institute(institute_id)
+);
+create table article_x_author(
+  person_id INT REFERENCES person(person_id),
+  article_id INT REFERENCES article(article_id)
+);
+create table article_x_magazine(
+  article_id INT references article(article_id),
+  magazine_id INT REFERENCES magazine(magazine_id)
+);
+create table article_x_reviewer(
+  article_id INT references article(article_id),
+  person_id INT REFERENCES person(person_id)
+);
+create table reviewer_x_magazine(
+  person_id INT REFERENCES person(person_id),
+  magazine_id INT REFERENCES magazine(magazine_id)
+);
---
+++ fillingdata.sql
@@ -0,0 +1,178 @@
+set search_path TO coursework, public;
+
+insert into institute(institute_id, institute_address_txt, institute_site_url, institute_email_txt, institute_phone_no)
+ values(0, null, 'www.kipt.ru', 'info@kipt.ru', '+74954084554');
+insert into institute(institute_id, institute_address_txt, institute_site_url, institute_email_txt, institute_phone_no)
+ values(1, 'Russia, Kudikina Gora, Main Street, 2', null, 'info@baba_yaga.dxdy', null);
+insert into institute(institute_id, institute_address_txt, institute_site_url, institute_email_txt, institute_phone_no)
+ values(2, null, 'www.hogvards.com', 'magic@hogvards.com', '+38-29344-883-7');
+insert into institute(institute_id, institute_address_txt, institute_site_url, institute_email_txt, institute_phone_no)
+ values(3, 'Dreamland, Sweet Street, 18', 'www.dreamuniversity.com', 'info@dreamuniversity.ru', '+88005553535');
+insert into institute(institute_id, institute_address_txt, institute_site_url, institute_email_txt, institute_phone_no)
+ values(4, 'Italy, Rome, 13-st street, 14', 'www.holyinstitute.in', 'faint@holyinstitute.in', null);
+
+insert into magazine(magazine_id, article_nm, magazine_address_txt, magazine_site_url, magazine_email_txt, magazine_phone_no, magazine_owner_nm)
+values (0, 'Horns && Hooves', 'London, 34-th street, 12', 'www.hornsandhooves.com', 'horns@hooves.com', '+3-43-988-7732', 'Mister Bychara');
+insert into magazine(magazine_id, article_nm, magazine_address_txt, magazine_site_url, magazine_email_txt, magazine_phone_no, magazine_owner_nm)
+values (1, 'Cosmo Chronicles', 'London, 34-th street, 13', 'www.cchonicles.com', 'cchronicles@gmail.com', '+3-23-768-7733', 'Mister Bychara Mladshiy');
+insert into magazine(magazine_id, article_nm, magazine_address_txt, magazine_site_url, magazine_email_txt, magazine_phone_no, magazine_owner_nm)
+values (2, 'Physicist Solving A City', 'France, De jour prospect, 345/2', null, 'info@psac.com', '+32-389447221', 'Augustin Louis Cauchy');
+insert into magazine(magazine_id, article_nm, magazine_address_txt, magazine_site_url, magazine_email_txt, magazine_phone_no, magazine_owner_nm)
+values (3, 'Theocracy Records', 'Italy, Rome, 13-st street, 13,', null, 'trecords@gmail.com', '+98-345-6677-23', 'Jorge Mario Bergoglio');
+insert into magazine(magazine_id, article_nm, magazine_address_txt, magazine_site_url, magazine_email_txt, magazine_phone_no, magazine_owner_nm)
+values (4, 'Goddamn Theories', null, 'www.godtheories.th', 'godtheories@gmail.com', null, null);
+
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (0, 'Jack Willson', 34, '+88889139045', 'jack.we@scoltech.ru', true, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (1, 'Peppy Dlinny Chulok', 19, null, 'leftred_rightblue@shworksward.mo', false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (2, 'John The Baptist', null, null, 'sainjohn@gmail.ru', true, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (3, 'Marry The Witch', null, '+0134-3445-123', 'slyGirl@yahoo.com', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (4, 'Sancho Panza', 74, null, 'sancho@skytech.ru', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (5, 'Angeline De Sorondan', 18, null, 'angel@gmail.com', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (6, 'Michael Pavlovich', 22, '+73432112343', 'arch_michael@mail.ru', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (7, 'Ozmo La Bonsheet', 97, null, 'oldsheeet@gmail.com', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (8, 'Ganz Christian', 23, '+83(343)34313', null, true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (9, 'Sofa Expert', 13, '+88888888888', 'love_smeshariki@mail.ru', true, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (10, 'Julia De Pompur', 17, null, 'julie_foreach@ozzi.fe', false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (11, 'Anna Nor Freuchtangerst De Prussiland', 43, '+73-9133-0021', 'annaFreuch@noble.ge', false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (12, 'Senior Junior', 27, null, 'serialkiller@gmail.com', false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (13, 'Kori Polska', 103, '03-13445-2343', 'koridepol@gmail.com', false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (14, 'Miss Slikworm', 44, '34456634313', null, false, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (15, 'Krosh', 19, '483189394221', 'zayc@smeshariki.com', true, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (16, 'Losyash', 20, '32443942221', 'los@smeshariki.com', true, true);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (17, 'A Huli', 2304, null, 'wisewerewolf@gmal.com', true, true); /*The name taken from Pelevin`s book 'Sacred book of the werewolf'*/
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (18, 'Mister Bychara', 58, '+34-341431-344', 'bigman@chick.com', false, false);
+insert into person(person_id, person_nm, person_age, person_phone_no, person_email_txt, person_reviewer_flg, person_author_flg)
+values (19, 'Senior Buro The Brave', null, null, 'havesomeguts@gmail.com', false, false);
+
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (0, 'Theory Of Anime Mass', 'Japanese Culture', 344, '2018-09-03');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (1, 'Modern application of Chulochnay theory', 'Dress Theory', 14, '2012-03-06');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (2, 'Introduction to green-landscape theory', 'Nature', 42, '1034-09-02');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (3, 'Theory Of Laziness', 'Feelings', 1, '2000-01-02');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (4, 'Cultural Revolution vs Great October Revoltuin', 'History', 23, '2002-03-23');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (5, 'Application Of Pristalniy Gaze method', 'Math', 24, '2012-02-03');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (6, 'Introduction to Irregular Carrot Arithmetics', 'Math', 58, '2012-09-03');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (7, 'The Holy Podstava theory in Theory of Laziness', 'Feelings', 20, '2018-09-03');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (8, 'The Everchanging Wind', 'Nature', 23, '2008-12-03');
+insert into article(article_id, article_nm, article_subject_txt, article_number_of_pages_num, article_date_of_publication_dt)
+values (9, 'Advanced chapters of Theory of Laziness', 'Feelings', 45, '2019-05-19');
+
+insert into author_person(person_id, institute_id) values (0, 0);
+insert into author_person(person_id, institute_id) values (1, 2);
+insert into author_person(person_id, institute_id) values (2, 1);
+insert into author_person(person_id, institute_id) values (11, 4);
+insert into author_person(person_id, institute_id) values (12, 0);
+insert into author_person(person_id, institute_id) values (13, 2);
+insert into author_person(person_id, institute_id) values (14, 2);
+insert into author_person(person_id, institute_id) values (15, 3);
+insert into author_person(person_id, institute_id) values (16, 3);
+insert into author_person(person_id, institute_id) values (17, 4);
+
+insert into article_x_author (person_id, article_id) VALUES (0,0);
+insert into article_x_author (person_id, article_id) VALUES (1,1);
+insert into article_x_author (person_id, article_id) VALUES (2,2);
+insert into article_x_author (person_id, article_id) VALUES (11,3);
+insert into article_x_author (person_id, article_id) VALUES (12,4);
+insert into article_x_author (person_id, article_id) VALUES (13,5);
+insert into article_x_author (person_id, article_id) VALUES (16,6);
+insert into article_x_author (person_id, article_id) VALUES (17,7);
+insert into article_x_author (person_id, article_id) VALUES (1,7);
+insert into article_x_author (person_id, article_id) VALUES (2,7);
+insert into article_x_author (person_id, article_id) VALUES (12,5);
+
+insert into article_x_magazine (article_id, magazine_id) values (0, 0);
+insert into article_x_magazine (article_id, magazine_id) values (1, 2);
+insert into article_x_magazine (article_id, magazine_id) values (2, 1);
+insert into article_x_magazine (article_id, magazine_id) values (3, 4);
+insert into article_x_magazine (article_id, magazine_id) values (4, 3);
+insert into article_x_magazine (article_id, magazine_id) values (5, 1);
+insert into article_x_magazine (article_id, magazine_id) values (6, 2);
+insert into article_x_magazine (article_id, magazine_id) values (7, 3);
+insert into article_x_magazine (article_id, magazine_id) values (8, 4);
+insert into article_x_magazine (article_id, magazine_id) values (9, 1);
+
+
+
+insert into article_x_reviewer (article_id, person_id) values (0, 17);
+insert into article_x_reviewer (article_id, person_id) values (0, 16);
+insert into article_x_reviewer (article_id, person_id) values (1, 15);
+insert into article_x_reviewer (article_id, person_id) values (2, 9);
+insert into article_x_reviewer (article_id, person_id) values (3, 8);
+insert into article_x_reviewer (article_id, person_id) values (4, 7);
+insert into article_x_reviewer (article_id, person_id) values (5, 6);
+insert into article_x_reviewer (article_id, person_id) values (6, 5);
+insert into article_x_reviewer (article_id, person_id) values (7, 4);
+insert into article_x_reviewer (article_id, person_id) values (8, 3);
+insert into article_x_reviewer (article_id, person_id) values (8, 2);
+insert into article_x_reviewer (article_id, person_id) values (8, 0);
+
+insert into reviewer_x_magazine (person_id, magazine_id) values (9, 0);
+insert into reviewer_x_magazine (person_id, magazine_id) values (17, 1);
+insert into reviewer_x_magazine (person_id, magazine_id) values (16, 2);
+insert into reviewer_x_magazine (person_id, magazine_id) values (15, 3);
+insert into reviewer_x_magazine (person_id, magazine_id) values (9, 4);
+insert into reviewer_x_magazine (person_id, magazine_id) values (8, 0);
+insert into reviewer_x_magazine (person_id, magazine_id) values (7, 1);
+insert into reviewer_x_magazine (person_id, magazine_id) values (6, 2);
+insert into reviewer_x_magazine (person_id, magazine_id) values (5, 1);
+insert into reviewer_x_magazine (person_id, magazine_id) values (4, 3);
+insert into reviewer_x_magazine (person_id, magazine_id) values (0, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
